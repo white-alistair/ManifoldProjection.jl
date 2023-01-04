a160166f9de5d4268e9f53efea8d92bbe74ef89c
Alistair White <alistair@pik-potsdam.de>
2023-01-04 18:00:54 +0100

    Refactor

--- src/coordinate_projection.jl
+++ src/coordinate_projection.jl
@@ -13,19 +13,42 @@ function CoordinateProjection(
     CoordinateProjection{R,J,T,Int64}(residual_function, residual_jacobian, tolerance, 10)
 end
 
-function coordinate_projection(
-    u0::Vector{T},
-    residual_function::Function,  # Function of u only
-    jac::AbstractMatrix{T},       # Jacobian evaluated at (u0, t0)
-    tolerance::T,
-    maxiters::Integer,
-) where {T}
-    N = size(residual_function(u0))[1]     # Number of invariants
+# function coordinate_projection(
+#     u0::Vector{T},
+#     residual_function::Function,  # Function of u only
+#     jac::AbstractMatrix{T},       # Jacobian evaluated at (u0, t0)
+#     tolerance::T,
+#     maxiters::Integer,
+# ) where {T}
+#     N = size(residual_function(u0))[1]     # Number of invariants
+#     λ = zeros(T, N)                        # One Lagrange multiplier for each invariant
+#     u = u0                                 # u will be the projection of u0 onto the manifold
+
+#     for _ = 1:maxiters
+#         residuals = residual_function(u)
+#         if all(abs.(residuals) .< tolerance)
+#             return u
+#         end
+#         λ -= inv(jac * jac') * residuals
+#         u = vec(u0 + jac' * λ)
+#     end
+
+#     @warn "maxiters exceeded for manifold projection, returning unprojected value"
+#     return u0
+# end
+
+function (cp::CoordinateProjection{R,J,T,M})(u0::Vector{T}, t0::T) where {R,J,T,M}
+    (; residual_function, residual_jacobian, tolerance, maxiters) = cp
+
+    get_residuals = u -> residual_function(u, t0)
+    jac = residual_jacobian(u0, t0)        # For efficiency we only evaluate the Jacobian at u0
+
+    N = size(get_residuals(u0))[1]         # Number of invariants
     λ = zeros(T, N)                        # One Lagrange multiplier for each invariant
     u = u0                                 # u will be the projection of u0 onto the manifold
 
     for _ = 1:maxiters
-        residuals = residual_function(u)
+        residuals = get_residuals(u)
         if all(abs.(residuals) .< tolerance)
             return u
         end
@@ -35,11 +58,4 @@ function coordinate_projection(
 
     @warn "maxiters exceeded for manifold projection, returning unprojected value"
     return u0
-end
-
-function (cp::CoordinateProjection{R,J,T,M})(u0::Vector{T}, t0::T) where {R,J,T,M}
-    (; residual_function, residual_jacobian, tolerance, maxiters) = cp
-    res = u -> residual_function(u, t0)
-    jac = residual_jacobian(u0, t0)
-    return coordinate_projection(u0, res, jac, tolerance, maxiters)
 end
