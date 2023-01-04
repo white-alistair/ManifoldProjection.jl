29f031958f721c0127accbf59adfc6d5457972dd
Alistair White <alistair@pik-potsdam.de>
2023-01-04 17:52:46 +0100

    Tidy up

--- src/coordinate_projection.jl
+++ src/coordinate_projection.jl
@@ -1,12 +1,16 @@
-struct CoordinateProjection{C<:Function,J<:Function,T<:AbstractFloat,M<:Integer}
-    residual_function::C
-    jacobian::J
+struct CoordinateProjection{R<:Function,J<:Function,T<:AbstractFloat,M<:Integer}
+    residual_function::R
+    residual_jacobian::J
     tolerance::T
     maxiters::M
 end
 
-function CoordinateProjection(constraints::C, jacobian::J, tolerance::T) where {C,J,T}
-    CoordinateProjection{C,J,T,Int64}(constraints, jacobian, tolerance, 10)
+function CoordinateProjection(
+    residual_function::R,
+    residual_jacobian::J,
+    tolerance::T,
+) where {R,J,T}
+    CoordinateProjection{R,J,T,Int64}(residual_function, residual_jacobian, tolerance, 10)
 end
 
 function coordinate_projection(
@@ -33,12 +37,9 @@ function coordinate_projection(
     return u0
 end
 
-function (cp::CoordinateProjection{C,J,T,M})(
-    u0::Vector{T},
-    t0::T,
-) where {C,J,T,M}
-    (; residual_function, jacobian, tolerance, maxiters) = cp
+function (cp::CoordinateProjection{R,J,T,M})(u0::Vector{T}, t0::T) where {R,J,T,M}
+    (; residual_function, residual_jacobian, tolerance, maxiters) = cp
     res = u -> residual_function(u, t0)
-    jac = jacobian(u0, t0)
+    jac = residual_jacobian(u0, t0)
     return coordinate_projection(u0, res, jac, tolerance, maxiters)
 end
